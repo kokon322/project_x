8562a1925f559eee1f2b83ffac702b8590bc8538
kokon322 <chornuy322@gmail.com>
2022-08-12 20:33:35 -0300

    fix

--- src/__tests__/app.module.spec.ts
+++ src/__tests__/app.module.spec.ts
@@ -1,13 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AppModule', () => {
   let testModule: TestingModule;
+  let mockTypeOrmModule;
+  let mockMongooseModule;
+  let mockConfigModule;
 
   beforeEach(async () => {
     testModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(TypeOrmModule)
+      .useValue(mockTypeOrmModule)
+      .overrideProvider(MongooseModule)
+      .useValue(mockMongooseModule)
+      .overrideProvider(ConfigModule)
+      .useValue(mockConfigModule)
+      .compile();
   });
 
   afterAll(async () => {
