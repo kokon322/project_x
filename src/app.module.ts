633bf82881838df670c65faffc955558047a921b
kokon322 <chornuy322@gmail.com>
2022-08-12 21:10:07 -0300

    add test controller to check CI\CD

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './db/PostgresConfigService';
 import { MongooseConfigService } from './db/MongooseConfigService';
+import { TestController } from './test/test.controller';
 
 @Module({
   imports: [
@@ -19,7 +20,7 @@ import { MongooseConfigService } from './db/MongooseConfigService';
       inject: [MongooseConfigService],
     }),
   ],
-  controllers: [],
+  controllers: [TestController],
   providers: [],
 })
 export class AppModule {
