819492ed3181fc834cc2977ae513047bc3fda8c5
kokon322 <chornuy322@gmail.com>
2022-09-01 11:20:29 -0300

    add files for auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  constructor() {
+  }
+  //Registration
+  //Log-In
+  //Log-Out
+  //Update-Tokens
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  constructor() {
+  }
+}
+//Registration
+//Log-In
+//Log-Out
+//Update-Tokens
